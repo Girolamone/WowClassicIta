79cf13ee5597aed1a8547fa658f4216d1aab4d1d
Spartaco Amadei <spartaco.amadei@outlook.it>
2025-05-08 19:47:56 +0200

    feat: enhance widget documentation and add global enable/disable button functionality

--- Core.lua
+++ Core.lua
@@ -3,7 +3,7 @@
 -- as the second argument.
 local addonName, addonTable = ...
 
---- @class WowItaWidgets
+--- @class WowItaWidgets Holds references to main addon widgets
 --- @field questFrameToggle table The frame of the button
 --- @field questLogFrameToggle table The frame of the quest log button
 --- @field gossipFrameToggle table The frame of the gossip button
@@ -15,6 +15,7 @@ local addonName, addonTable = ...
 
 --- @class WowClassicIta : AceAddon-3.0, AceConsole-3.0, AceEvent-3.0, AceHook-3.0, AceTimer-3.0
 --- @field db table A reference to persistent saved val in DB
+--- @field db.profile WowClassicItaSettings The current profile settings
 --- @field options table Options table for the add-on
 --- @field configFrame table The configuration frame for the add-on
 --- @field default table Default settings for the add-on
@@ -89,7 +90,7 @@ end
 
 --- When Addon is enabled
 function WowClassicIta:OnEnable()
-    self:Trace("[Core:OnEnable()] Addon correctly loaded and enabled!")
+    self:Trace("|cFFFFC0CB[Core:OnEnable()]|r Addon correctly loaded and enabled!")
 
     --@do-not-package@
     --- Debug porpuse buttons (that not will be available in the release)
@@ -99,6 +100,8 @@ function WowClassicIta:OnEnable()
     local reloadUiButton = LibStub("AceGUI-3.0"):Create("Button")
     ---@type AceGUIButton
     local resetDbButton = LibStub("AceGUI-3.0"):Create("Button")
+    ---@type AceGUIButton
+    local globalExtOnOffButton = LibStub("AceGUI-3.0"):Create("Button")
 
     reloadUiButton:SetWidth(100)
     reloadUiButton:SetHeight(22)
@@ -117,6 +120,21 @@ function WowClassicIta:OnEnable()
         self:Print("Database resetted!")
     end)
 
+    globalExtOnOffButton:SetWidth(100)
+    globalExtOnOffButton:SetHeight(22)
+    globalExtOnOffButton:SetText("Enable/Disable addon")
+    self.addonOnOffSwitch = self:FetchCurrentSetting().disabled
+    globalExtOnOffButton:SetCallback("OnClick", function()
+        if not self.addonOnOffSwitch then
+            self:TurnAddonOn()
+        else
+            self:TurnAddonsOff()
+        end
+        self.addonOnOffSwitch = not self.addonOnOffSwitch
+        self.db.profile.disabled = self.addonOnOffSwitch
+        self:Info("Add-on is now " .. (self.addonOnOffSwitch and "disabled" or "enabled"))
+    end)
+
     ---@diagnostic disable-next-line: invisible
     debugButtonsContainer.frame:SetPoint("TOPRIGHT", QuestFrame, "TOPRIGHT", 520, 100)
     debugButtonsContainer:SetFullWidth(true)
@@ -124,6 +142,7 @@ function WowClassicIta:OnEnable()
     debugButtonsContainer:SetLayout("Flow")
     debugButtonsContainer:AddChild(reloadUiButton)
     debugButtonsContainer:AddChild(resetDbButton)
+    debugButtonsContainer:AddChild(globalExtOnOffButton)
     debugButtonsContainer:DoLayout()
     --@end-do-not-package@
 
@@ -135,13 +154,13 @@ function WowClassicIta:OnEnable()
         self:Error("If you want, open an issue at: " .. addonTable.GitHubURL)
         self:UnregisterAllEvents()
         return
-    elseif (not questFrameIdButton or not questLogIdButton) and self.db.profile.quests.enabled and not self.db.profile.disabled then
+    elseif (not questFrameIdButton or not questLogIdButton) and self:FetchCurrentSetting().quests.enabled and not self:FetchCurrentSetting().disabled then
         self:Error("Impossible to create the quest/questlog frame button!")
         self:Error("Extension cannot be loaded, so will be disabled.")
         self:Error("If you want, open an issue at: " .. addonTable.GitHubURL)
         self:UnregisterAllEvents()
         return
-    elseif not gossipIdButton and self.db.profile.gossips.enable and not self.db.profile.disabled then
+    elseif not gossipIdButton and self:FetchCurrentSetting().gossips.enable and not self:FetchCurrentSetting().disabled then
         self:Error("Impossible to create the gossip frame button!")
         self:Error("Extension cannot be loaded, so will be disabled.")
         self:Error("If you want, open an issue at: " .. addonTable.GitHubURL)
@@ -171,7 +190,7 @@ function WowClassicIta:OnEnable()
     }
 
     local onSelectQuestLogEntry = function()
-        self:Trace("[Core:onSelectQuestLogEntry] Fired function handler")
+        self:Trace("|cFFFFC0CB[Core:onSelectQuestLogEntry]|r Fired function handler")
         local questID = self:GetQuestID()
         if not questID then
             --- If the quest ID is not available, log an error
@@ -192,13 +211,13 @@ function WowClassicIta:OnEnable()
 
     --QuestLogDetailScrollFrame:HookScript("OnShow", onQuestLogEntryClick)
     --[[EmptyQuestLogFrame:HookScript("OnShow", function()
-        self:Trace("[Core:EmptyQuestLogFrame:Hooks:OnShow] EmptyQuestLogFrame hoocked OnShow() started!")
+        self:Trace("|cFFFFC0CB[Core:EmptyQuestLogFrame:Hooks:OnShow]|r EmptyQuestLogFrame hoocked OnShow() started!")
         self.questLogIdButton:Hide()
     end)]]
     --self:SecureHook('SelectQuestLogEntry', onSelectQuestLogEntry)
     self:SecureHook('SelectQuestLogEntry', function ()
-        self:Trace("[Core:Hooks:SelectQuestLogEntry] Quest Log Entry clicked!")
-        if not self.db.profile.quests.enabled or self.db.profile.disable then
+        self:Trace("|cFFFFC0CB[Core:Hooks:SelectQuestLogEntry]|r Quest Log Entry clicked!")
+        if not self:FetchCurrentSetting().quests.enabled or self:FetchCurrentSetting().disable then
             --- If the quest translation is disabled, return without doing anything.
             self:Debug("Quest translation is disabled, so return without doing anything.")
             return
@@ -219,17 +238,16 @@ function WowClassicIta:OnEnable()
         self.characterRace = UnitRace("player")
         self.characterSex = UnitSex("player")
 
-        self:Debug("Current logged user information:")
-        self:Debug("Player name: " .. self.characterName)
-        self:Debug("Player class: " .. self.characterClass)
-        self:Debug("Player race: " .. self.characterRace)
-        self:Debug("Player sex: " .. self.characterSex)
+        self:Debug("|cFFDDA0DDPlayer name:|r " .. self.characterName)
+        self:Debug("|cFFDDA0DDPlayer class:|r " .. self.characterClass)
+        self:Debug("|cFFDDA0DDPlayer race:|r " .. self.characterRace)
+        self:Debug("|cFFDDA0DDPlayer sex:|r " .. self.characterSex)
     end)
 
     --- the player progress inside a quest.
     self:RegisterEvent('QUEST_PROGRESS', function()
-        self:Trace("[Core:RegisterEvents(QUEST_PROGRESS)] event fired!")
-        if not self.db.profile.quests.enabled then
+        self:Trace("|cFFFFC0CB[Core:RegisterEvents(QUEST_PROGRESS)]|r event fired!")
+        if not self:FetchCurrentSetting().quests.enabled then
             --- If the quest translation is disabled, return without doing anything.
             return
         end
@@ -254,8 +272,8 @@ function WowClassicIta:OnEnable()
 
     --- the player get details from a quest.
     self:RegisterEvent('QUEST_DETAIL', function()
-        self:Trace("[Core:RegisterEvent(QUEST_DETAIL)] event fired!")
-        if not self.db.profile.quests.enabled then
+        self:Trace("|cFFFFC0CB[Core:RegisterEvent(QUEST_DETAIL)]|r event fired!")
+        if not self:FetchCurrentSetting().quests.enabled then
             --- If the quest translation is disabled, return without doing anything.
             return
         end
@@ -275,13 +293,14 @@ function WowClassicIta:OnEnable()
         self:UpdateQuestFrame({
             --Title = self:GetQuestTitle(questID),
             Description = self:GetQuestDescription(questID),
+            Objectives = self:GetQuestObjectives(questID),
         })
     end);
 
     --- the player complete a quest.
     self:RegisterEvent('QUEST_COMPLETE', function()
-        self:Trace("[Core:RegisterEvent(QUEST_COMPLETE)] event fired!")
-        if not self.db.profile.quests.enabled then
+        self:Trace("|cFFFFC0CB[Core:RegisterEvent(QUEST_COMPLETE)]|r event fired!")
+        if not self:FetchCurrentSetting().quests.enabled then
             --- If the quest translation is disabled, return without doing anything.
             return
         end
@@ -306,7 +325,7 @@ function WowClassicIta:OnEnable()
 
     --[[
     self:RegisterEvent("GOSSIP_SHOW", function()
-        if not self.db.profile.gossips.enabled then
+        if not self:GetSetting().gossips.enabled then
             --- If the gossip translation is disabled, return without doing anything.
             return
         end
